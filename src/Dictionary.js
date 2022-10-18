357a4a4fc276ef9d83ce518154847032ec4405d0
rcontini <rachelecontini5@gmail.com>
2022-10-17 21:37:23 -0400

    created new state, created new component with meanings to keywords

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -28,6 +30,7 @@ export default function Dictionary() {
           onChange={handleKeyword}
         ></input>
       </form>
+      <Results results={results} />
     </div>
   );
 }
