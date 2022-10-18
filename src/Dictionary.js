159d3750db728b59c5d04b937b92c88b532d6007
rcontini <rachelecontini5@gmail.com>
2022-10-17 20:00:54 -0400

    console.log

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,7 +6,7 @@ export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
   function handleResponse(response) {
-    console.log(response);
+    console.log(response.data);
   }
 
   function search(event) {
