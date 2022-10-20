c169ba9f49288f5a2de020fac803d1f149f67433
rcontini <rachelecontini5@gmail.com>
2022-10-20 18:26:20 -0400

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   console.log(props.meaning);
@@ -11,6 +12,7 @@ export default function Meaning(props) {
             <p>
               {definition.definition} <br />
               <em>{definition.example}</em>
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
