0c242326c971467ce75ade653e9db0ec8067c68b
rcontini <rachelecontini5@gmail.com>
2022-10-22 20:35:50 -0400

    trying to solve photo issue

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -6,7 +6,6 @@ export default function Phonetic(props) {
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-
       {props.phonetic.text}
     </div>
   );
