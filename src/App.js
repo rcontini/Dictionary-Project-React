cb0603e76cf2b2122e0747786231185f5ed53398
rcontini <rachelecontini5@gmail.com>
2022-10-23 12:56:06 -0400

    added links to footer and made description more desriptive for SEO

--- src/App.js
+++ src/App.js
@@ -9,7 +9,21 @@ export default function App() {
 
         <Dictionary />
 
-        <footer className="App-footer">Coded by Rachele Contini</footer>
+        <footer className="App-footer">
+          <a
+            href="https://github.com/rcontini/Dictionary-Project-React"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open-source code
+          </a>{" "}
+          by Rachele Contini for
+          <a href="https://www.shecodes.io/" target="_blank" rel="noreferrer">
+            {" "}
+            SheCodes
+          </a>
+        </footer>
       </div>
     </div>
   );
